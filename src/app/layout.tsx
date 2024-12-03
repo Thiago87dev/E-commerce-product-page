934c6940f0c4060e26836dc5ce559f428f5b80c9
thiago alves <thiago87gsa@gmail.com>
2024-12-03 14:34:27 -0300

    :tada: First commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const KumbhSans = localFont({
+  src: [
+    { path: "./fonts/KumbhSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/KumbhSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+  variable: "--font-kumbh-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${KumbhSans.className}  antialiased`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        colorOrange: 'hsl(26, 100%, 55%)',
+        colorPaleOrange: 'hsl(25, 100%, 94%)',
+        colorVeryDarkBlue: 'hsl(220, 13%, 13%)',
+        colorDarkGrayishBlue:' hsl(219, 9%, 45%)',
+        colorGrayishBlue: 'hsl(220, 14%, 75%)',
+        colorLightGrayishBlue: 'hsl(223, 64%, 98%)',
+        colorWhite: 'hsl(0, 0%, 100%)',
+        colorBlack75opacity: 'hsla(0, 0%, 0%, 0.75)',
       },
     },
   },
